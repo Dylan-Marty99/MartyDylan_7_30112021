e6bbf5fe183c6a9c6eebac71d22974d4804b260f
Dylan-Marty99 <dylan.marty99@gmail.com>
2022-01-06 09:07:47 +0100

    Début style home page

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -3,8 +3,27 @@ import LeftNavBar from "../components/LeftNavBar";
 
 const Home = () => {
   return (
-    <div className="profil-page">
+    <div className="home-page">
       <LeftNavBar />
+      <div className="message-container">
+        <div className="new-message">
+          <h2>Partagez quelque chose</h2>
+          <input type="text" placeholder="Écrivez quelque chose" />
+          <button>Envoyer</button>
+        </div>
+        <div className="all-post">
+          <div className="post">
+            <div className="post-title">
+              <h2>"Titre du post"</h2>
+              <span>"date du post"</span>
+            </div>
+            <span>"Nom de l'utilisateur"</span>
+            <p>"Contenu du post"</p>
+            <p>"image"</p>
+            <img src="" alt="" />
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
--- frontend/src/pages/Profil.js
+++ frontend/src/pages/Profil.js
@@ -10,7 +10,12 @@ const Profil = () => {
       <LeftNavBar />
       <h1>Votre profil</h1>
       <div className="profil-container">
-        
+        <p>Votre nom : "nom prenom"</p>
+        <p>Votre pseudo : "pseudo"</p>
+        <p>Votre secteur d'activité : "input liste déroulante"</p>
+        <p>Votre date d'inscription : "date"</p>
+        {/* modif couleur button */}
+        <button>Supprimer votre compte</button>
       </div>
     </div>
   );
