8a55a3a0bc8bad1339d091372208c75f4df36529
Dylan-Marty99 <dylan.marty99@gmail.com>
2022-01-06 10:55:46 +0100

    Suite home page

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -8,17 +8,47 @@ const Home = () => {
       <div className="message-container">
         <div className="new-message">
           <h2>Partagez quelque chose</h2>
-          <input type="text" placeholder="Écrivez quelque chose" />
+          <input type="text" placeholder="Écrivez quelque chose !" />
           <button>Envoyer</button>
         </div>
+
         <div className="all-post">
           <div className="post">
             <div className="post-title">
               <h2>"Titre du post"</h2>
               <span>"date du post"</span>
             </div>
-            <span>"Nom de l'utilisateur"</span>
-            <p>"Contenu du post"</p>
+            <span className="user-name">Par "Nom de l'utilisateur"</span>
+            <p className="content">
+              "Contenu du post" Lorem ipsum dolor sit amet consectetur
+              adipisicing elit. Quod odit sapiente iure nulla eum assumenda sed
+              ex? Id, consequuntur, voluptate excepturi quis rerum dignissimos
+              illum, neque esse obcaecati ipsam dolorum. Eum adipisci obcaecati
+              autem repellat, exercitationem praesentium facere odio quaerat,
+              perspiciatis officiis earum molestias deleniti ipsam provident? Ex
+              vitae beatae nisi architecto. Accusamus consequatur doloremque
+              incidunt quasi nulla repellat atque.
+            </p>
+            <p>"image"</p>
+            <img src="" alt="" />
+          </div>
+
+          <div className="post">
+            <div className="post-title">
+              <h2>"Titre du post"</h2>
+              <span>"date du post"</span>
+            </div>
+            <span className="user-name">"Nom de l'utilisateur"</span>
+            <p className="content">
+              "Contenu du post" Lorem ipsum dolor sit amet consectetur
+              adipisicing elit. Ea explicabo ipsa totam autem, perspiciatis rem
+              eos id quasi temporibus sapiente possimus voluptas quae dolorum
+              libero dicta repellendus vero! Voluptatem, ab? Officiis nostrum
+              quaerat quo ipsam expedita quasi nobis voluptate officia
+              voluptatum id, reiciendis quod ullam debitis cupiditate
+              perspiciatis maxime iste? Modi repudiandae veniam blanditiis iusto
+              officia inventore perferendis, sit at.
+            </p>
             <p>"image"</p>
             <img src="" alt="" />
           </div>
